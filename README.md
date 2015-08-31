57698c56c3e0c4a92d7e4a665573a10539e7503f
mayth <chimeaquas@hotmail.com>
2015-09-01 05:38:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+decyber
+=======
+
+**why CTF teams/organizers need a cyber-theme?**
+
+decyber is a theme for [Hugo](http://gohugo.io).
+
+# Installation
+
+Add this repository as a git submodule to your repository.
+
+# Copyright
+
+Copyright (c) 2015 mayth (Mei Akizuru)
+
+This theme is provided under the MIT License (See LICENSE.md).
+
+This theme uses the following assets. They are licensed by its copyright holders:
+
+* [Pure](http://purecss.io)
+* [jQuery](http://jquery.com)
+
+Notice: To simplify the installation process, they are included in this repository.
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    mainBowerFiles = require('main-bower-files'),
+    bowerNormalizer = require('gulp-bower-normalize'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task('bower', function () {
+  return gulp.src(mainBowerFiles(), { base: './bower_components/' })
+    .pipe(bowerNormalizer({ flatten: true }))
+    .pipe(gulp.dest('./assets'));
+});
+
+gulp.task('uglify', function () {
+  return gulp.src('./assets/js/**/*.js')
+    .pipe(concat('theme.js'))
+    .pipe(uglify({ preserveComments: 'license' }))
+    .pipe(gulp.dest('./static/js/'));
+});
+
+gulp.task('minify-css', function () {
+  gulp.src('./assets/css/**/*.css')
+    .pipe(concat('app.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('dist', ['uglify', 'minify-css']);
